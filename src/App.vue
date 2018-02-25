37e8585e5088683622daff2427d3c236e348c86d
Taylor Dondich <taylor@dondich.com>
2018-02-25 14:29:56 -0800

    Add initial rom loader component. Initial parsing of some ines header data.

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,32 @@
     <cpu-2a03 ref="cpu" />
     <!-- Bring in our memory component -->
     <memory size="0xffff" ref="memory" />
+    <!-- Bring in our rom loader -->
+    <rom-loader  ref="loader" />
+
   </div>
 </template>
 
 <script>
 import cpu2a03 from './components/cpu-2a03.vue'
 import memory from './components/memory.vue';
+import romLoader from './components/rom-loader.vue';
 
 export default {
   name: 'app',
   components: {
     'cpu-2a03': cpu2a03,
-    'memory': memory
+    'memory': memory,
+    'rom-loader': romLoader
   }
 }
 </script>
 
-<style lang="sass">
+<style lang="scss">
 @import '~bootstrap/scss/bootstrap.scss';
+
+body {
+  padding-bottom: 32px;
+}
 
 </style>
---
+++ src/components/rom-loader.vue
@@ -0,0 +1,145 @@
+<template>
+<div class="row">
+    <div class="col-sm-12">
+       <h2>ROM Cartridge Loader</h2> 
+       <div class="alert alert-danger" v-if="loadError">
+           {{loadError}}
+       </div>
+       <div class="alert alert-success" v-if="loadSuccess">
+           {{loadSuccess}}
+       </div>
+       <p>
+       <input v-model="romName"> <button @click="load">Load ROM</button>
+       </p>
+       <table class="table table-dark table-sm" v-if="loadSuccess">
+            <thead>
+                <tr>
+                    <th>PRG Rom Size</th>
+                    <th>CHR Rom Size</th>
+                    <th>Mirroring</th>
+                    <th>Battery Backed</th>
+                    <th>Trainer Exists</th>
+                    <th>Ignore Mirroring</th>
+                    <th>Mapping Number</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{prgRomSize}}</td>
+                    <td>{{chrRomSize}}</td>
+                    <td>{{mirroring}}</td>
+                    <td>{{batteryBacked}}</td>
+                    <td>{{trainerExists}}</td>
+                    <td>{{ignoreMirroring}}</td>
+                    <td>{{mappingNumber}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+  
+</template>
+
+<script>
+import axios from 'axios';
+
+/**
+Builds a String from a DataView (backed by an ArrayBuffer)
+ */
+function getStringFromDataView(view, offset = 0, length = view.byteLength - offset) {
+    let value = new String();
+    for(let count = 0; count < length; count++) {
+        value = value + String.fromCharCode(view.getInt8(offset + count));
+    }
+    return value;
+}
+
+export default {
+
+    data: function() {
+        return {
+            loadError: false,
+            loadSuccess: false,
+            data: null,
+            romName: ''
+        }
+    },
+    computed: {
+        // Computed characteristics of the rom after being loaded
+        // See: http://wiki.nesdev.com/w/index.php/INES
+        prgRomSize() {
+            return this.data[4];
+        },
+        chrRomSize() {
+            return this.data[5];
+        },
+        mirroring() {
+            return ((this.data[6] & 0b00000001) == 0b00000001) ? 'vertical' : 'horizontal';
+        },
+        batteryBacked() {
+            return (this.data[6] & 0b00000010) == 0b00000010;
+        },
+        trainerExists() {
+            return (this.data[6] & 0b00000100) == 0b00000100;
+        },
+        ignoreMirroring() {
+            return (this.data[6] & 0b00001000) == 0b00001000;
+        },
+        mappingNumber() {
+            // Combine the nibbles of byte 6 and 7
+            console.log("Original 6:" + this.data[6].toString(2).padStart(8, '0'));
+            console.log("Original 8:" + this.data[6].toString(2).padStart(8, '0'));
+
+            let lower = this.data[6] >>> 4;
+            let upper = this.data[7] & 0b11110000;
+            console.log("Lower: " + lower.toString(2));
+            console.log("Upper: " + upper.toString(2));
+            let mapping = upper | lower;
+            console.log("Mapping: " + mapping.toString(2) + " or " + mapping);
+            return mapping;
+        },
+        vsSystem() {
+            return (this.data[8] & 0b00000001) == 0b00000001;
+        },
+        playChoice10() {
+            return (this.data[8] & 0b00000010) == 0b00000010;
+        },
+        nes2Format() {
+            return (this.data[8] & 0b00000100) == 0b00000100;
+        }
+    },
+    methods: {
+        load() {
+            this.loadError = this.loadSuccess = false;
+            axios.get('/roms/' + this.romName + '.nes', {
+                responseType: 'arraybuffer'
+            })
+            .then((response) => {
+                // The response.data property is the arraybuffer of our binary data
+                console.log("Retrieved rom.");
+                // We assume this is a iNES file.
+                // See: http://wiki.nesdev.com/w/index.php/INES
+
+                // Fetch header from iNES file
+                let header = new DataView(response.data, 0, 16);
+                if(getStringFromDataView(header, 0, 3) != 'NES') {
+                    this.loadError = "Invalid rom file provided";
+                    return;
+                }
+                // Now assign to our data
+                this.data = new Uint8Array(response.data);
+                this.loadSuccess = "Loaded ROM";
+            })
+            .catch((error) => {
+                this.loadError = error.status + ": " + error.statusText;
+            });
+        }
+    }
+  
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
+
